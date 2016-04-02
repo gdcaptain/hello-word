94fdec1c0f513e900b5a3c51f3b549626e243e21
gdcaptain <sdwl613@163.com>
2016-04-02 16:26:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,17 @@
 <title>hello word</title>
 </head>
 <body>
+<div alian=center>
+<h2>留言本</h2>
 <a href="#">查看留言</a>
 <a href="#">删除留言</a>
 <br>
+<hr>
+<form action = submitform.php method=post>
+姓名：<input type="text" name="name">
+邮箱：<input type="email" name="email">
+<input type=submit>
+</form>
+</div>
 </body>
 </html>
