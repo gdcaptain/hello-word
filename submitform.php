ae923b302f8ff00e93174399f54a0dd94d1aada5
gdcaptain <sdwl613@163.com>
2016-04-03 00:28:00 +0800

    Update submitform.php

--- submitform.php
+++ submitform.php
@@ -1,6 +1,21 @@
+<html>
+<head>
+<title>hello word</title>
+<link rel="stylesheet" href="lyb.css" type="text/css"></link>
+</head>
+<body>
+<div id="luiyanben">
+<h2>留言本</h2>
+<a href="#">查看留言</a>
+<a href="#">删除留言</a>
+<br>
 <?php
-
-
-
-
+echo "Hello world!<br>";
+$name=$_POST['name'];
+$email=$_POST['email'];
+echo "your name is {$name}"."<br>";
+echo "your email is {$email} <br>";
+print ('感谢!');
 ?>
+</body>
+</html>
