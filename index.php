9067ea1899126f192532ce5a60948fab93b87bad
gdcaptain <sdwl613@163.com>
2016-04-03 00:28:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <html>
 <head>
 <title>hello word</title>
+<link rel="stylesheet" href="lyb.css" type="text/css"></link>
 </head>
 <body>
-<div alian=center>
+<div id="luiyanben">
 <h2>留言本</h2>
 <a href="#">查看留言</a>
 <a href="#">删除留言</a>
 <br>
 <hr>
-<form action = submitform.php method=post>
+<form action ="sub1.php" method="post">
 姓名：<input type="text" name="name">
 邮箱：<input type="email" name="email">
-<input type=submit>
+<input type="submit">
 </form>
 </div>
 </body>
